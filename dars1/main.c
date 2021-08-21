d04b3d31d9bfe5d3082df685b8689c8d75dab85a
akbarjondev <akbarjon.tojiev@gmail.com>
2021-08-21 08:13:24 +0500

    Add data types

--- dars1/main.c
+++ dars1/main.c
@@ -4,5 +4,18 @@
 int main()
 {
     printf("Salom dunyo!\n");
+
+    char belgi = 'A';
+    printf("Ismimning boshi %c dan boshlanadi.\n", belgi);
+
+    char belgilarSeriyasi[] = "Abutech komaniyasi";
+    printf("Bizning %s.\n", belgilarSeriyasi);
+
+    int projects = 5;
+    printf("Loyihalar %d\n", projects);
+
+    float balls = 10.5;
+    printf("Ballari: %.2f\n", balls);
+
     return 0;
 }
